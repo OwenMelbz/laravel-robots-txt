ed1cd0bf02c58cbf8df4022cf966393374057edb
Owen <owenmelbz@gmail.com>
2017-05-27 10:41:07 +0100

    removed unneeded comment

--- src/RobotsTxtServiceProvider.php
+++ src/RobotsTxtServiceProvider.php
@@ -13,10 +13,7 @@ use Illuminate\Support\ServiceProvider;
 class RobotsTxtServiceProvider extends ServiceProvider {
 
     /**
-     * This will be used to register config & view in
-     * your package namespace.
-     *
-     * --> Replace with your package name <--
+     * This will be used to register config & view
      */
     protected $packageName = 'robots_txt';
 
